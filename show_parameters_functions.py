31076ec4b00eabe8e315e68dc847b283215db34f
Kiril Lazarov <kirillazarov@abv.bg>
2024-09-22 10:12:53 +0300

    Init commit. This module contains the functions responsible for displaying the parameters on the screen.

---
+++ show_parameters_functions.py
@@ -0,0 +1,21 @@
+import pygame 
+
+
+def show_parameters(params_layer, font_small, const_params_dict,
+                    var_params_dict):
+    text_x = 20
+    text_y = 20
+    for param, value in const_params_dict.items():
+        curr_value = value + var_params_dict[param]
+        text = font_small.render(f'{param}: {curr_value:.2f}', True, (0, 0, 0))
+        
+        params_layer.blit(text, (text_x, text_y))
+        text_y += 20
+        
+def update_var_params_dict(var_params_dict, v_additional, w_additional, x_additional, k_additional):
+    
+    var_params_dict = {'v': v_additional, 
+                        'w': w_additional, 
+                       'k': k_additional, 
+                       'x': x_additional} 
+    return var_params_dict
