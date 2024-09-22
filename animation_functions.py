28f3e5420bc4d0fd381e869543b605d09890978a
Kiril Lazarov <kirillazarov@abv.bg>
2024-09-23 00:46:37 +0300

    Clean the code in create_background(), delete input variables in calc_spiral_coordinate() and replace them with max_spiral_vector_length, and set the positions of the numbers on the number line in draw_x_axis() and draw_y_axis().

--- animation_functions.py
+++ animation_functions.py
@@ -3,13 +3,12 @@ import numpy as np
 
 
 def create_background(background_surface,screen_width, screen_height, units, length, bg_color, font_small, after_stop = False):
+    
+
 
     line_color = (200,200,200)
     center_point = (screen_width/2, screen_height/2)
 
-    markers = []   
-    
-    
     # Coordinate x-line parameters
     x_line_start = 0
     x_line_end = screen_width
@@ -23,11 +22,7 @@ def create_background(background_surface,screen_width, screen_height, units, len
     
     x_line = screen_width/2
     
-    markers.append({
-        'receiver_x': x_line_end,
-        'receiver_y': y_line,
-    })
-    
+
     
     pygame.draw.line(background_surface, color=line_color, start_pos=(x_line_start, y_line),
                          end_pos = (x_line_end, y_line), width=2)
@@ -50,18 +45,7 @@ def create_background(background_surface,screen_width, screen_height, units, len
      
     number_x = horizontal_line_end + 15
         
-    screen_objects = {
-    'x_line_start': x_line_start,
-    'vertical_line_start': vertical_line_start,
-    'vertical_line_end': vertical_line_end,
-    'x_line_end': x_line_end,
-    'y_line': y_line,
-    'vertical_line_x': [],
-    'numbers': [],
-    'signals_start_positions': {},
-    'spaceship_last_position': None
-    }
-  
+   
     half_units = (units)/2
 
     for i in range(int(half_units)):
@@ -76,13 +60,12 @@ def create_background(background_surface,screen_width, screen_height, units, len
                 i, length, y_line_start,horizontal_line_start,
                 horizontal_line_end, line_color,font_small, half_units,number_x,pos_number, neg_number)
 
-    return background_surface, markers, screen_objects
-
 
 
 def draw_x_axis_values(background_surface, screen_width, screen_height,
                 i, length, x_line_start,vertical_line_start,
                 vertical_line_end, line_color,font_small, half_units,number_y,pos_number, neg_number):
+    
     vertical_line_x_neg = x_line_start + i*length
     vertical_line_x_pos = screen_width/2 + i*length
 
@@ -93,14 +76,20 @@ def draw_x_axis_values(background_surface, screen_width, screen_height,
     #Draw unit lines for positive x-axis
     pygame.draw.line(background_surface, color = line_color, start_pos=(vertical_line_x_pos, vertical_line_start),
                  end_pos = (vertical_line_x_pos, vertical_line_end), width=1)  
-    
-    #Draw negative numbers
-    neg_number_text = font_small.render(f'-{neg_number}', True, line_color)
-    background_surface.blit(neg_number_text, (vertical_line_x_neg-12,number_y-4)) 
-    
-    #Draw positive numbers
-    pos_number_text = font_small.render(f'{pos_number}', True, line_color)
-    background_surface.blit(pos_number_text, (vertical_line_x_pos-12,number_y-4))
+
+    #Draw zero
+    if i == 0:
+        pos_number_text = font_small.render(f'{pos_number}', True, line_color)
+        background_surface.blit(pos_number_text, (vertical_line_x_pos-15,number_y-4))
+        
+    else:
+        #Draw negative numbers
+        neg_number_text = font_small.render(f'-{neg_number}', True, line_color)
+        background_surface.blit(neg_number_text, (vertical_line_x_neg-12,number_y-4)) 
+
+        #Draw positive numbers
+        pos_number_text = font_small.render(f'{pos_number}', True, line_color)
+        background_surface.blit(pos_number_text, (vertical_line_x_pos-5,number_y-4))
 
     
     
@@ -120,33 +109,32 @@ def draw_y_axis_values(background_surface, screen_width, screen_height,
              end_pos = (horizontal_line_end, horizontal_line_y_pos), width=1)
 
     if i >=1:
-        if i <10:
-            #Draw negative numbers
-            neg_number_text = font_small.render(f'-{pos_number}', True, line_color)
-            background_surface.blit(neg_number_text, (number_x-50,horizontal_line_y_neg)) 
+       
+        #Draw negative numbers
+        neg_number_text = font_small.render(f'-{pos_number}', True, line_color)
+        background_surface.blit(neg_number_text, (number_x-50,horizontal_line_y_neg-5)) 
 
-            #Draw positive numbers
-            neg_number_text = font_small.render(f'{pos_number}', True, line_color)
-            background_surface.blit(neg_number_text, (number_x-50,horizontal_line_y_pos))
-
-        else:
-            #Draw negative numbers
-            neg_number_text = font_small.render(f'-{pos_number}', True, line_color)
-            background_surface.blit(neg_number_text, (number_x-57,horizontal_line_y_neg)) 
-
-            #Draw positive numbers
-            neg_number_text = font_small.render(f'{pos_number}', True, line_color)
-            background_surface.blit(neg_number_text, (number_x-57,horizontal_line_y_pos))
+        #Draw positive numbers
+        neg_number_text = font_small.render(f'{pos_number}', True, line_color)
+        background_surface.blit(neg_number_text, (number_x-50,horizontal_line_y_pos-5))
+        
+        
             
 def x_transform(x, screen_width,length):
+    
     return x* length + screen_width/2
 
 def y_transform(y, screen_height,length):
+    
     return -y * length + screen_height /2
 
 
 def draw_vertical_line(line_layer, screen_width, screen_height, length, units,x_axis_value=1):
+    
+    line_layer.fill((0, 0, 0, 0))
+    
     half_units = units/2
+    
     x_up, y_up  = x_transform(x_axis_value, screen_width, length),x_transform(half_units, screen_width, length)
     x_down, y_down = x_transform(x_axis_value, screen_width, length),x_transform(-half_units, screen_width, length)
     
@@ -154,14 +142,12 @@ def draw_vertical_line(line_layer, screen_width, screen_height, length, units,x_
     
     
        
-def calc_spiral_coord(screen_width, screen_height, length, units, v=1, w=1, k=0):
+def calc_spiral_coord(max_spiral_vector_length, units, v=1, w=1, k=0):
     
     theta_0 = k * np.pi/2
     
     half_units = units /2
-    
-    # N = half_units/(v*2*np.pi)
-    
+
     # Needed time t for one rotation
     rotation_t = 2 * np.pi/(abs(w))
     
@@ -170,30 +156,14 @@ def calc_spiral_coord(screen_width, screen_height, length, units, v=1, w=1, k=0)
     one_rotation_rad_vec_len = v * rotation_t
     
     N = half_units / one_rotation_rad_vec_len
-    
-    max_spiral_vector_length = np.sqrt(screen_width ** 2 + screen_height ** 2)/length
-    
-#     print('max_spiral_vector_length: ', max_spiral_vector_length)
-#     print('max_spiral_vector_t: ', max_spiral_vector_length,max_spiral_vector_length/v )
-    
-#     print('rotation_t: ', rotation_t)
-#     print('one_rotation_rad_vec_len: ', one_rotation_rad_vec_len)
+
     max_t = max_spiral_vector_length/N
-    # print('Max t: ', max_t)
+
     lin_space = (0, max_t)
     
-    num = int(max_t*100)
-    
-    # print('num: ', num)
-    # print('N: ', N)
+    num = 2000
+
     T = np.linspace(*lin_space, num)
-    # print('Len T before: ', len(T))
-    T = np.array([t for t in T if t * v < max_spiral_vector_length])
-    # print('Len T after: ', len(T))
-    # print('lin_space: ', lin_space)
-    # print('Len lin_space: ', len(lin_space))
-    # print('len T: ', len(T))
-    # print(T)
     
     angles = np.array([theta_0 + t * w for t in T])
     radiuses = np.array([v * t for t in T])
@@ -201,11 +171,12 @@ def calc_spiral_coord(screen_width, screen_height, length, units, v=1, w=1, k=0)
     x = radiuses * np.cos(angles)
     y = radiuses * np.sin(angles)
     
-    return x, y
+    return x, y, 
 
-def draw_spiral(spiral_layer, screen_width, screen_height,length,units, v=1,w=1,k=0):
+def draw_spiral(spiral_layer, screen_width, screen_height,length,max_spiral_vector_length, units, v=1,w=1,k=0):
     
-    x_spiral, y_spiral = calc_spiral_coord(screen_width, screen_height,length,units, v=v,w=w,k=k)
+    spiral_layer.fill((0, 0, 0, 0))
+    x_spiral, y_spiral = calc_spiral_coord(max_spiral_vector_length,units, v=v,w=w,k=k)
     
     x_spiral = [x_transform(x, screen_width, length) for x in x_spiral]
     y_spiral = [y_transform(y, screen_height, length) for y in y_spiral]
--- show_parameters_functions.py
+++ show_parameters_functions.py
@@ -1,16 +1,28 @@
 import pygame 
-
+import numpy as np
 
 def show_parameters(params_layer, font_small, const_params_dict,
                     var_params_dict):
+    
+    params_layer.fill((0, 0, 0, 0))  
+    
     text_x = 20
     text_y = 20
-    for param, value in const_params_dict.items():
+    for param, value in const_params_dict.items():  
+            
         curr_value = value + var_params_dict[param]
         text = font_small.render(f'{param}: {curr_value:.2f}', True, (0, 0, 0))
         
         params_layer.blit(text, (text_x, text_y))
         text_y += 20
+
+        if param == 'k':
+            theta_0 = (value + var_params_dict[param]) * (np.pi /2) * 180 / np.pi 
+            text = font_small.render(f'theta_0: {theta_0:.2f}', True, (0, 0, 0))
+            
+            params_layer.blit(text, (text_x, text_y))
+            text_y += 20
+        
         
 def update_var_params_dict(var_params_dict, v_additional, w_additional, x_additional, k_additional):
     
