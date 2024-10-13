9cecab3eceb2ca5320709099cb2002be4de96466
Kiril Lazarov <kirillazarov@abv.bg>
2024-10-14 01:30:47 +0300

    Restoring get_mth_aproximation() with the previous calculation of the spiral coordinates.

--- formula_functions.py
+++ formula_functions.py
@@ -92,37 +92,39 @@ def get_nth_intersect(n, k, w):
 
 
 
-def get_mth_aproximation(t_nth, x_line, deg, v, w, k, i, accuracy, correction_mech, f_binary=False):
-    
+def get_mth_aproximation(t_nth, x_line, v, w, k, i=200, accuracy=5, correction_mech=False, f_binary=False):
+
     t_0 = np.copy(t_nth)
     init_theta_angle = k * np.pi/2
-    
+    # deg = 0
+
     init_y = get_y_coord(v, w, k, t_0)
-    
+    # init_y = get_nth_deg_y_derivative(deg, t_0, v, w, k)
+
     # a_coeff = float(round(A_coeff(x_line, w, v, k, init_y),5))
     a_coeff = A_coeff(x_line, w, v, k, init_y)
-    
-   
-    
-    # x_coeff = X_bin(x_line)
-    diffs_massive = []
-    
-    last_t = np.copy(t_0)
-    
 
-    
-    for m in range(1, i):
-  
-        # curr_x = get_x_coord(v, w, k, t_0)
-        # curr_y = get_y_coord(v, w, k, t_0)
-        
-        curr_x = get_nth_deg_x_derivative(deg,t_0, v,w,k)
-        curr_y= get_nth_deg_y_derivative(deg,t_0, v,w,k)
-        
-        
+
+
+    # x_coeff = X_bin(x_line)
+    # diffs_massive = []
+
+    last_t = np.copy(t_0)
+
+
+
+    for _ in range(1,i):
+
+        curr_x = get_x_coord(v, w, k, t_0)
+        curr_y = get_y_coord(v, w, k, t_0)
+
+        # curr_x = get_nth_deg_x_derivative(deg,t_0, v,w,k)
+        # curr_y= get_nth_deg_y_derivative(deg,t_0, v,w,k)
+
+
         if f_binary:
             x_bin = X_bin(abs(x_line) - abs(curr_x))
-  
+
 
             t_ternary = T_ternary(abs(x_line), abs(curr_x), x_bin)
             g_bin = G_bin(t_ternary)
@@ -137,13 +139,14 @@ def get_mth_aproximation(t_nth, x_line, deg, v, w, k, i, accuracy, correction_me
             c = abs(x_line) - abs(curr_x)
 
         a = np.sqrt(x_line**2 + curr_y**2)
-        
-        b = v*t_0 # Current radius vector
-        
+
+        # b = v*t_0 # Current radius vector
+        b = np.sqrt(curr_x ** 2 + curr_y**2)
+
         cos_delta_phi = (a ** 2 + b**2 - c ** 2)/(2 * a * b)
-        
-        
-   
+
+
+
         if abs(cos_delta_phi) > 1:
 
             cos_delta_phi = 1
@@ -151,15 +154,15 @@ def get_mth_aproximation(t_nth, x_line, deg, v, w, k, i, accuracy, correction_me
         delta_phi = np.arccos(cos_delta_phi)
 
         curr_t = (a_coeff * delta_phi) / abs(w)
-  
+
         if c == 0:
-              
+
             t_0 +=curr_t*(-1)
-     
+
         else:
             '(x_line/abs(x_line))*'
             t_0 += (c/abs(c))*curr_t 
-    
+
         statement = f'{t_0:.{accuracy}f}' == f'{last_t:.{accuracy}f}'
 
         if statement:
