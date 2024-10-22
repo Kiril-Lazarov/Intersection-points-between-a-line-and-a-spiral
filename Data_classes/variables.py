ba662ec8c181b78e14803b7b8ce574e2153ef779
Kiril Lazarov <kirillazarov@abv.bg>
2024-10-22 17:42:51 +0300

    Added logic for changing the step of the variables - new class variables _factor have been added to a new dictionary factors_dict, and logic has been added to reset_dicts() to reset them to their initial values.

--- Data_classes/variables.py
+++ Data_classes/variables.py
@@ -17,40 +17,57 @@ class Variables(Data):
     l_additional =  0
     c_additional = [0, 0] # Center point additional values - [width, height]
 
+    t_factor = 1
+    v_factor = 1
+    w_factor = 1
+    x_factor = 1
+    k_factor = 1
+    l_factor = 1
     
     def __init__(self):
         super().__init__()
+        
         self.variables_dict = {}
+        self.factors_dict = {}
         self.excluded_methods_names = [name[0] for name in inspect.getmembers(Variables, predicate=inspect.isfunction)]\
                                     + ['_abc_impl'] + ['class_init_values']
 
     @property
     def class_init_values(self):
         
-        return [self.deg_additional, self.t_additional, self.v_additional, 
+        return ([self.deg_additional, self.t_additional, self.v_additional, 
                 self.w_additional, self.x_additional, self.k_additional, 
-                self.l_additional, self.c_additional]
+                self.l_additional, self.c_additional],
+                
+                [None, self.t_factor, self.v_factor, self.w_factor, self.x_factor, self.k_factor, self.l_factor, None])
     
     
     def create_dict(self):        
         
         for name, value in Variables.__dict__.items():
             if not name.startswith('__') and name not in self.excluded_methods_names:
+      
+                if '_additional' in name:
+                    reduced_name = name.split('_additional')[0]
+                    self.variables_dict[reduced_name] = value
+                    
+                elif '_factor' in name:
+                    reduced_name = name.split('_factor')[0]
+                    self.factors_dict[reduced_name] = value
                 
-                reduced_name = name.split('_additional')[0]
-                self.variables_dict[reduced_name] = value
                 
     def reset_dict(self):
 
         index = 0
         for key in self.variables_dict.keys():
 
-            if key == 'c':
+            if key == 'c' or key == 'deg':
 
                 self.variables_dict[key] = [0, 0]
             else:
 
-                self.variables_dict[key] = self.class_init_values[index]
+                self.variables_dict[key] = self.class_init_values[0][index]
+                self.factors_dict[key] = self.class_init_values[1][index]
 
             index += 1
 
