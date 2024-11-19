e2187d344aebc0f0e227ee9490d9a9d2582345e0
Kiril Lazarov <kirillazarov@abv.bg>
2024-11-19 18:05:21 +0200

    New formula for calculating the initial angle to the ordinate axis in get_delta_theta().

--- formula_functions.py
+++ formula_functions.py
@@ -2,6 +2,7 @@ import numpy as np
 # from Data_classes.data_processing import DataProcessing
 
 def D_bin(k):
+
     return (1 + (-1) ** np.floor(k+1))/2
 
 def derivative_change(x_0, x_t):
@@ -29,11 +30,19 @@ def derivative_change(x_0, x_t):
 
 
 def W_bin(w):
-    return (1 - w / abs(w))/2
+
+    return (1 - w / abs(X_bin(w)))/2
 
 
 def B_bin(k):
+    
+#     if int(k) == 1 or int(k) == 3:
+    
+#         if len(str(k)) > 2:
+#             k = float(str(k)[:-1])
+
     return np.ceil(k) - np.floor(k)
+
 
 def calc_power(k, w):
     
@@ -53,10 +62,12 @@ def calc_power(k, w):
 
 def KWX_line(k, w, x_line):
     
-    # print('k = 0.9999999999999999 : ', k==0.9999999999999999 )
-    
-    # if k==0.9999999999999999:
-    #     k = 1
+#     if len(str(k)) > 2:
+#         k = float(str(k)[:-1])
+        
+#     if len(str(w)) > 2:
+#         w = float(str(w)[:-1])
+        
     k_coeff = ((k-1)/abs(X_bin(k-1)) * ((3-k)/abs(X_bin(3-k))))                         
     
     if k_coeff > 0:
@@ -68,52 +79,60 @@ def KWX_line(k, w, x_line):
 
     k13_coeff = (1 - abs(k_coeff))
 
-#     w_coeff = w / abs(X_bin(w))
+    w_coeff = w / abs(X_bin(w))
     
-#     if w_coeff > 0:
-#         w_coeff = 1
-#     elif w_coeff < 0:
-#         w_coeff = -1
-#     else:
-#         w_coeff = 0
+    if w_coeff > 0:
+        w_coeff = 1
+    elif w_coeff < 0:
+        w_coeff = -1
+    else:
+        w_coeff = 0
         
-#     power = calc_power(k, w)
+    # power = calc_power(k, w)
         
-#     w_special_cases = w_coeff ** (power)
+    # w_special_cases = w_coeff ** (power)
     
-#     # print('k: ', k, 'w_coeff: ', w_coeff, 'power: ',power, 'w_special_cases: ',w_special_cases)
-#     x_line_coeff = x_line/(abs(X_bin(x_line)))
+    # print('k: ', k, 'w_coeff: ', w_coeff, 'power: ',power, 'w_special_cases: ',w_special_cases)
+    x_line_coeff = x_line/(abs(X_bin(x_line)))
     
-#     if x_line_coeff > 0:
-#         x_line_coeff = 1
-#     elif x_line_coeff < 0:
-#         x_line_coeff = -1
-#     else:
-#         x_line_coeff = 0  
+    if x_line_coeff > 0:
+        x_line_coeff = 1
+    elif x_line_coeff < 0:
+        x_line_coeff = -1
+    else:
+        x_line_coeff = 0  
   
-    # return np.floor((1 - k13_coeff *w_special_cases*x_line_coeff)/2)
-    return k13_coeff
+    return np.floor((1 - k13_coeff *w_coeff*x_line_coeff)/2) 
+    # return k13_coeff
 
-def swap_result(value, constant):
+def swap_result(value, constant, direction=False):
     
     v_c_sum = value - constant
+    
+    x_bin = X_bin(v_c_sum) if not direction else abs(X_bin(v_c_sum))
 
-    return 1- ((v_c_sum)/ X_bin(v_c_sum))
+    return 1- ((v_c_sum)/x_bin)
+
 
 def special_cases_factor(n, k, w):
     
-    
+    if len(str(k)) > 2:
+        k = float(str(k)[:-1])
+        
+    if len(str(w)) > 2:
+        w = float(str(w)[:-1])
+    # print()
     n_koeff = swap_result(n, 1)
-
+    # print('k: ', k)
     k_1_koeff = swap_result(k, 1)
-
+    # print('k: ', k, 'k_1_koeff: ', k_1_koeff)
     w_koeff = swap_result(w, 1) 
 
     k_3_koeff =swap_result(k, 3)
 
     neg_w_koeff = swap_result(w, -1)
-
-    factor = (1-n_koeff*k_1_koeff*neg_w_koeff) + (1-n_koeff*k_3_koeff*w_koeff) 
+    # print('neg_w_koeff: ', neg_w_koeff)
+    factor = (1-n_koeff*k_1_koeff*w_koeff) + (1-n_koeff*k_3_koeff*neg_w_koeff) 
     
     factor = np.floor(factor/2)
 
@@ -122,7 +141,11 @@ def special_cases_factor(n, k, w):
 
 def K_sign(k, x_line):
     
+#     if len(str(k)) > 2:
+#         k = float(str(k)[:-1])
+    
     k_coeff = ((k-1)/abs(X_bin(k-1))) * ((k-3)/abs(X_bin(k-3)))
+   
     
     if k_coeff > 0:
         k_coeff = 1
@@ -170,18 +193,30 @@ def get_delta_theta_plus(k):
     
 
 def get_delta_theta(w, k):
-    
-    W_coeff = W_bin(w)
-    B_coeff = B_bin(k)
-    
-    delta_plus = get_delta_theta_plus(k)
-    
-    return W_coeff * B_coeff * (np.pi - 2 * delta_plus) + delta_plus
+
+    b_bin = B_bin(np.floor(k)/2)
+    w_bin= w/abs(X_bin(w))
+
+    k_1 = (k-1)/ X_bin(k-1)
+    k_3 = (k-3)/ X_bin(k-3)
+    # print(k_1*k_3)
+
+    function = w_bin*(np.floor(k+ (1 + w_bin)/2) - k)
+    addition_term = b_bin * np.floor((1 + w_bin)/2) + (1 - b_bin) * np.floor((1 - w_bin)/2)
+    print()
+    print('function: ', function)
+    print('addition_term: ', addition_term)
+
+    # exclude_coeff = np.floor((1-B_bin(np.floor(k)/2) + w/abs(X_bin(w)))/2)
+    final = k_1*k_3*(function + addition_term) + (1-k_1*k_3) *w/X_bin(w)* 2
+    return final * np.pi/2 
+    # return  W_coeff * B_coeff * (np.pi - 2 * delta_plus) + delta_plus
     
     
 
 def X_bin(x):
-    return (x ** (0 ** abs(x) - 1)) ** -1
+    # return (x ** (0 ** abs(x) - 1)) ** -1
+    return x ** (1 - 0**abs(x))
 
 def get_y_coord(v, w, k, prev_t):
     return v * prev_t * np.sin(k*np.pi/2 + w*prev_t)
@@ -206,7 +241,7 @@ def get_nth_deg_x_derivative(deg,t, v,w,k):
     nth_sin_deriv = trig_derivatives_cycle[next_nth]
     
     return v * w ** (deg-1) * (deg * nth_cos_deriv + w*t * nth_sin_deriv)
-    # return cos_theta
+
 
 def get_nth_deg_y_derivative(deg, t, v, w, k):
     theta = k*np.pi/ 2 + w * t
@@ -261,27 +296,40 @@ def get_nth_intersect(data_processing, n, w,k, final_solution = False):
     first_y_rad_vec = get_nth_deg_y_derivative(deg_y, first_y_t, v, w, k)
     
     x_max_dist = x_max_line(first_y_rad_vec, x_line)
+    
+    special_cases = special_cases_factor(n, k, w)
+    
+    # print('special_cases: ',special_cases)
+  
     # print()
-    # print('k_sign: ',k_sign)
-    # print('kwx_coeff: ',kwx_coeff)
-    # # print('x_max_dist* is_der_changed* (k_sign + kwx_coeff): ', x_max_dist* is_der_changed* (k_sign + kwx_coeff))
-    # print('(k_sign + kwx_coeff): ', (k_sign + kwx_coeff))
-    # # print('is_der_changed: ', is_der_changed)
-    # # print('x_max_dist: ',x_max_dist)
-    # # print()
+    print('k: ',k, 'w: ', w, 'x: ',x_line)
+    print('delta theta: ', delta_theta)
+    print('k_sign: ',k_sign)
+    print('kwx_coeff: ',kwx_coeff)
+    print('x_max_dist* is_der_changed* (k_sign + kwx_coeff): ', x_max_dist* is_der_changed* (k_sign + kwx_coeff))
+    print('(k_sign + kwx_coeff): ', (k_sign + kwx_coeff))
+    print('is_der_changed: ', is_der_changed)
+    print('x_max_dist: ',x_max_dist)
+#     # # print()
+    result = x_max_dist* is_der_changed* (k_sign + kwx_coeff)*(1 - n/X_bin(n)) * zero_y_t\
+                             + (n/X_bin(n))*(delta_theta + (n-1) * np.pi) / abs(w)
+    # print(f'Special cases factor at n = {n}, w={X_bin(w)}, k = {k} : ', special_cases)
 
-    return x_max_dist* is_der_changed* (k_sign + kwx_coeff)*(1 - n/X_bin(n)) * zero_y_t+ n/X_bin(n)*(delta_theta + (n-1) * np.pi) / abs(w)
+    return  result
 
 
 
 def get_mth_aproximation(data_processing, t_nth, i=200, accuracy=5, down_direction = False, correction_mech=False, f_binary=False):
 
     if t_nth > 0:
+        
         deg_x, deg_y = data_processing.get_curr_param('deg')
         v = data_processing.get_curr_param('v')
         w = data_processing.get_curr_param('w')
         k = data_processing.get_curr_param('k')
         x_line = data_processing.get_curr_param('x')
+        
+        n = data_processing.algorithm_vars.algorithm_vars_dict['n']
 
         t_0 = np.copy(t_nth)
         init_theta_angle = k * np.pi/2
@@ -290,12 +338,10 @@ def get_mth_aproximation(data_processing, t_nth, i=200, accuracy=5, down_directi
         
         init_x_derivative = get_nth_deg_x_derivative(deg_x+1, t_0, v, w, k)
         
-        # a_coeff = float(round(A_coeff(x_line, w, v, k, init_y),5))
         a_coeff = A_coeff(x_line, w, v, k, init_y)
 
 
         last_t = np.copy(t_0)
-
 
 
         for _ in range(1,i):
