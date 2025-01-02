ba4dd87041dbb3b5938c8d8eee6a473a34c60ace
Kiril Lazarov <kirillazarov@abv.bg>
2025-01-02 19:38:11 +0200

    Added load_images().

--- Paper/image_functions.py
+++ Paper/image_functions.py
@@ -11,9 +11,24 @@ def load_image(name=None):
     html_string = f"""
     <div style="display: flex; justify-content: center; align-items: center; height: 300px;">
         <img src="Images/{name}.png" style="width: 562px; height: 300px;">
-        <div style="position: absolute; top: 0px; right: 200px; color: black; font-size: 14px; background-color: rgba(255, 255, 255, 0.7); padding:           5px; border-radius: 5px;">
+        <div style="position: absolute; top: 0px; right: 80px; color: black; font-size: 14px; background-color: rgba(255, 255, 255, 0.7); padding:           5px; border-radius: 5px;">
             {caption_text}
         </div>
     </div>
     """
+    return HTML(html_string)
+
+def load_images(images):
+    html_string = "<div style='display: flex; justify-content: space-between;'>"
+    for name, position in images:
+        caption_text = get_caption_text(name)
+        html_string += f"""
+        <div style="text-align: center; margin: 0 10px;">
+            <img src="Images/{name}.png" style="width: 375px; height: 200px;">
+            <div style="color: black; font-size: 14px; background-color: rgba(255, 255, 255, 0.7); padding: 5px; border-radius: 5px;">
+                {caption_text}
+            </div>
+        </div>
+        """
+    html_string += "</div>"
     return HTML(html_string)
